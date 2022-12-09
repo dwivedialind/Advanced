5f625f77808b0f44e9326186253e73fdbedd8716
dwivedialind <84173548+dwivedialind@users.noreply.github.com>
2022-12-09 11:38:31 +0530

    TreeNode class

---
+++ TreeNode.java
@@ -0,0 +1,14 @@
+package DSAAdvanced;
+
+
+
+public class TreeNode {
+   public TreeNode left;
+    public TreeNode right;
+    public int val;
+    public TreeNode(int x){
+        val=x;
+        left=null;
+        right =null;
+    }
+}
